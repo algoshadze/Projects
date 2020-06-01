b6216223e50c5500006872b516abbd149b612056
Alexander Goshadze <algoshadze@gmail.com>
2020-06-02 00:45:02 +0300

    Update Contacts.js

--- Pshychologist/frontend/src/components/scenes/Contacts.js
+++ Pshychologist/frontend/src/components/scenes/Contacts.js
@@ -1,40 +1,29 @@
 import React from 'react'
 
+import PhoneIcon from './phone.ico'
+
 function Contacts() {
   return (
     <div>
       <div >
         <h4>Адрес:</h4>
-        <ul>
-          <li class="address">
+
             <p>
-              <i class="icon-map-marker"></i>м. Шоссе Энтузиастов, Шоссе Энтузиастов 31с38 <br /> (Торговый центр 31, 1 минута от метро) <br />   Время работы 10:00 - 20:00  <br /> <br /> <br /> м. Славянский Бульвар, Можайское шоссе 30 <br /> (вход со двора, где на козырьке написано "МУЗМАГ") <br />  Время работы 11:00 - 22:00</p>
-          </li>
-        </ul>
+              <i class="icon-map-marker"></i>м. Славянский Бульвар, Можайское шоссе 30 <br /> (вход со двора, где на козырьке написано "МУЗМАГ") <br />  Время работы 11:00 - 22:00</p>
+
         <h4>Телефон:</h4>
-        <a href='#' class="icon-phone"></a> 8 (999) 842-05-45
-                <ul>
-          <li class="phone%">
-            <p>
-              <i class="icon-phone"></i><span>8 (964)</span> 566-10-00</p>
-          </li>
-        </ul>
-        <h4>По вопросу опта:</h4>
-        <ul>
-          <li class="phone%">
-            <p>
-              <i class="icon-phone"></i>8 (952) 243-8424</p>
-          </li>
-        </ul>
+
+        <a><img src={PhoneIcon} class="icon-phone"/> 8 (999) 842-05-45</a><br/>
+        <a><img src={PhoneIcon} class="icon-phone"/> 8 (964) 566-10-00 </a>
+        <h4>Звонить с 11 до 20</h4>
+
         <h4>E-mail</h4>
-        <ul>
-          <li class="mail">
+
             <p>
               <i class="icon-envelope"></i>
               <a style={{ fontsize: 14 }} href="mailto:privet@kupitegitaru.ru" onclick="ga('send', 'event', 'post_l', 'Click'); yaCounter34067245.reachGoal('post_l'); return true;">privet@kupitegitaru.ru</a>
             </p>
-          </li>
-        </ul>
+  
       </div>
     </div>
   )
